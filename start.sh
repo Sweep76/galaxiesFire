6e44413598d3fcd7c1c58496d35c90fe2d4d79fd
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-10 18:51:40 +0800

    initial setup

---
+++ start.sh
@@ -0,0 +1,7 @@
+npx create-expo-app galaxiesFire -t expo-template-blank-typescript
+cd ./galaxiesFire
+npx expo install firebase
+
+# Navigation
+npm install @react-navigation/native @react-navigation/native-stack
+npx expo install react-native-screens react-native-safe-area-context
