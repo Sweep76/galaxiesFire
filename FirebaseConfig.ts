94a7267a809ed779e7598130384e5cc2e2ea49db
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-10 19:00:20 +0800

    build: firebase config

---
+++ FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3hqsM0D5Zv8EVwOHkWcG3RfjKR89io6k",
+  authDomain: "rnauthvideo-bcc65.firebaseapp.com",
+  projectId: "rnauthvideo-bcc65",
+  storageBucket: "rnauthvideo-bcc65.firebasestorage.app",
+  messagingSenderId: "512331916515",
+  appId: "1:512331916515:web:2d5dc361f805e7758cd1d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
