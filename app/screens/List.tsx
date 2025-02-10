904da504ea2496a25ec0afff8ad6f13d6a4953c6
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-10 20:05:07 +0800

    feat: added list component

---
+++ app/screens/List.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class List extends Component {
+  render() {
+    return (
+      <View>
+        <Text>List</Text>
+      </View>
+    )
+  }
+}
+
+export default List
