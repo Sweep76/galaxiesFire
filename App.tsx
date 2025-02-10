41e685a0195eb8bc8be04b87af9362314749d3b0
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-10 20:26:49 +0800

    feat: navigation panel

--- App.tsx
+++ App.tsx
@@ -1,9 +1,26 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './app/screens/Login';
+import List from './app/screens/List';
+import Details from './app/screens/Details';
+import { useState } from 'react';
+import { User } from 'firebase/auth';
 
 const Stack = createNativeStackNavigator();
+
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
+    return (
+      <InsideStack.Navigator>
+        <InsideStack.Screen name="My todos" component={List} />
+        <InsideStack.Screen name="details" component={Details} />
+      </InsideStack.Navigator>
+    );
+}
+
 export default function App() {
+  const [user, setUser] = useState<User | null>(null);
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
