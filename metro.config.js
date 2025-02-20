3e17928dac00ccdb12dc1dc389e8168bdf2fdcd8
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-20 19:36:19 +0800

    fix: renamed getDefaultConfig to rerun bundle

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const getDefaultConfig = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname); // Use a different variable name
 defaultConfig.resolver.assetExts.push('cjs');
 
 module.exports = defaultConfig;
